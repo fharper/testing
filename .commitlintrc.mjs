3076a842932b5c10e762efdc36fecadfd422efd6
Frédéric Harper <hi@fred.dev>
2024-07-09 10:02:02 -0400

    check commit

---
+++ .commitlintrc.mjs
@@ -0,0 +1,19 @@
+const Configuration = {
+
+  extends: ['@commitlint/rules'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+  },
+  plugins: [
+    {
+      rules: {
+        'header-max-length': (parsed, _when, _value) => {
+          parsed.header = parsed.header.replace(/\s\(#[0-9]+\)$/, '')
+          return rules.default['header-max-length'](parsed, _when, _value)
+        },
+      },
+    },
+  ]
+};
+
+export default Configuration;
